7dce2b917dbe031a062bab61048b15e332aea415
Gabriele <gabriele.losi@outlook.com>
2020-10-28 10:53:03 +0100

    Started to prepare the script to test PES functions

--- triboflow/tests/HeteroStructureHS/run_PES.py
+++ triboflow/tests/HeteroStructureHS/run_PES.py
@@ -10,14 +10,20 @@ import numpy as np
 from PES_functions import *
 from utility_functions import *
 
-pes_array2 = np.genfromtxt('pes_clean.dat')
-pes_array = np.unique(pes_array2, axis=0)
+
+# Define input file containing the data, and the lattice cell vectors
+input_file = 'pes_clean.dat'
 cell = np.array([[  1.23817 ,   2.1466  ,   0.065971],
                  [  2.478096,   0.      ,   0.065971],
                  [  0.      ,   0.      , -46.575684]])
 
+# Clean the pes data
+pes_data = np.genfromtxt('pes_clean.dat')
+pes_data = np.unique(pes_data, axis=0)
+pbc = PBC_Coordinates(pes_data[:, :2], cell, to_array=True)
+pes_data[:, :2] = pbc.copy()
 
-data_rep = ReplicatePESPoints(pes_array, cell, replicate_of=(3, 3) )
+data_rep = ReplicatePESPoints(pes_data, cell, replicate_of=(3, 3) )
 
 
 
