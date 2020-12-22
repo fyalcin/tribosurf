93e839d1f51c94c2bf026f79d27b092790eda48c
mwolloch <michael.wolloch@univie.ac.at>
2020-12-22 16:59:42 +0100

    Changed the PES subworkflow test to test if stuff works with manually supplying slabs.

--- triboflow/tests/PES_test.py
+++ triboflow/tests/PES_test.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 19 16:15:02 2020
+Created on Mon Dec 21 14:53:59 2020
 
-@author: mwo
+@author: wolloch
 """
+
+from pymatgen.core.structure import Structure
+from pymatgen.core.surface import Slab
 from fireworks import LaunchPad
-from fireworks.core.rocket_launcher import rapidfire
-from triboflow.utils.database import GetDBJSON
 from triboflow.workflows.subworkflows import CalcPES_SWF
+from triboflow.utils.structure_manipulation import SlabFromStructure
+from triboflow.utils.database import GetDBJSON, GetSlabFromDB
 
+db_file = GetDBJSON()
+functional = "PBE"
+mpid = "mp-124"
+miller = [1,1,1]
 
+Slab_dict = GetSlabFromDB(mpid, db_file, miller, functional)
+slab = Slab.from_dict(Slab_dict['relaxed_slab'])
+comp_params = Slab_dict['comp_parameters']
 
-WF = CalcPES_SWF(interface_name='C001_Ni111_mp-23_mp-48',
-                  functional='PBE')
-
+WF = CalcPES_SWF(top_slab=slab, bottom_slab=slab, comp_parameters=comp_params)
 
 lpad = LaunchPad.auto_load()
 lpad.add_wf(WF)
-#rapidfire(lpad)
