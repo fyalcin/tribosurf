5ce20cd353b91bb900a39c5ffa76bd38c530e880
mwo <m.wolloch@gmail.com>
2020-12-23 16:11:47 +0100

    Added the test option of Graphene on Graphene for a very quick test.

--- triboflow/tests/PES_test.py
+++ triboflow/tests/PES_test.py
@@ -11,18 +11,31 @@ from pymatgen.core.surface import Slab
 from fireworks import LaunchPad
 from triboflow.workflows.subworkflows import CalcPES_SWF
 from triboflow.utils.structure_manipulation import SlabFromStructure
-from triboflow.utils.database import GetDBJSON, GetSlabFromDB
+from triboflow.utils.database import GetDBJSON, GetSlabFromDB, \
+    GetLowEnergyStructure
 
 db_file = GetDBJSON()
 functional = "PBE"
-mpid = "mp-124"
-miller = [1,1,1]
 
-Slab_dict = GetSlabFromDB(mpid, db_file, miller, functional)
-slab = Slab.from_dict(Slab_dict['relaxed_slab'])
-comp_params = Slab_dict['comp_parameters']
+#Test Ag111Ag111 interface
+# mpid = "mp-124"
+# miller = [1,1,1]
+# Slab_dict = GetSlabFromDB(mpid, db_file, miller, functional)
+# slab = Slab.from_dict(Slab_dict['relaxed_slab'])
+# comp_params = Slab_dict['comp_parameters']
 
-WF = CalcPES_SWF(top_slab=slab, bottom_slab=slab, comp_parameters=comp_params)
+#Test GrapheneGraphene interface
+struct, mpid = GetLowEnergyStructure('C', 'mp-1040425')
+slab = SlabFromStructure([0,0,1], struct)
+comp_params = {'functional': functional,
+               'use_vdw': True,
+               'use_spin': True,
+               'encut': 500,
+               'is_metal': True,
+               'k_dens': 2000}
+
+WF = CalcPES_SWF(top_slab=slab, bottom_slab=slab, comp_parameters=comp_params,
+                 file_output=True, output_dir='/home/mwo/FireWorks/test_dir')
 
 lpad = LaunchPad.auto_load()
 lpad.add_wf(WF)
