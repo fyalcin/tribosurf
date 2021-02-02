86cfc7d99c150fec3c4acac5ba5c8a503599fffc
Gabriele Losi <gabriele.losi@outlook.com>
2021-02-02 15:12:03 +0100

    Add schematic FTs for Surf.En. and Slabs Thickness

---
+++ triboflow/firetasks/slabs_wfs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 15:02:48 2021
+
+Classes to generate subworkflows for calculating slab properties.
+
+@author: glosi000
+"""
+
+__author__ = 'Gabriele Losi'
+__copyright__ = 'Prof. M.C. Righi, University of Bologna'
+__contact__ = 'clelia.righi@unibo.it'
+__date__ = 'February 2nd, 2021'
+
+
+class SlabSWFs:
+    """
+    Collection of static methods to manipulate slab structures.
+    
+    """
+    
+    @staticmethod
+    def converge_thickness_surfene():        
+        """
+        Method description
+        """
+        pass
+        
+    @staticmethod
+    def converge_thickness_alat():        
+        """
+        Method description
+        """
+        pass
---
+++ triboflow/firetasks/surfene.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 14:54:53 2021
+
+Firetasks to calculate the surface energy for a bulk and a given orientation.
+
+@author: glosi000
+"""
+
+__author__ = 'Gabriele Losi'
+__copyright__ = 'Prof. M.C. Righi, University of Bologna'
+__contact__ = 'clelia.righi@unibo.it'
+__date__ = 'February 2nd, 2021'
+
+from fireworks.utilities.fw_utilities import explicit_serialize
+from fireworks import FiretaskBase
+from atomate.utils.utils import env_chk
+
+from triboflow.utils.database import Navigator, NavigatorMP
+
+
+# ============================================================================
+# Firetasks
+# ============================================================================
+
+@explicit_serialize
+class FT_SurfaceEnergy(FiretaskBase):
+    """
+    Firetask description...
+    
+    """
+    
+    _fw_name = 'Surface Energy calculation'
+    required_params = ['mp_id', 'functional']
+    optional_params = ['db_file']
+    
+    def run_task(self, fw_spec):
+        pass
+
+
+# ============================================================================
+# Functions
+# ============================================================================
