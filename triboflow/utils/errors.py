698a032399c8c8589f9cd65e850e7633eff0810b
Gabriele Losi <gabriele.losi@outlook.com>
2021-02-09 10:47:33 +0100

    Move errors to utils and add new ones for Navigators classes

--- triboflow/utils/errors.py
+++ triboflow/utils/errors.py
@@ -8,13 +8,19 @@ Custom errors for any Firetasks and Workflow.
 @author: glosi000
 """
 
+__author__ = 'Gabriele Losi'
+__copyright__ = 'Prof. M.C. Righi, University of Bologna'
+__contact__ = 'clelia.righi@unibo.it'
+__date__ = 'February 8th, 2021'
+
+
 class SlabOptThickError(Exception): 
-    """ Error in Slab Optimal Thickness workflows and dependencies
+    """ Error in Slab Optimal Thickness workflows and dependencies.
     """
     pass
 
 class GenerateSlabsErrors(Exception):
-    """ Error in generating slabs
+    """ Error in generating slabs.
     """
     @staticmethod
     def check_thickness(thickness):
@@ -27,15 +33,26 @@ class GenerateSlabsErrors(Exception):
         if not isinstance(slab_name, (str, list)):
             raise GenerateSlabsErrors("Wrong type for argument: slab_name. "
                                       "Allowed types: list of str, str.")
-        elif not all([isinstance(x, str) for x in list(slab_name]):
+        elif not all([isinstance(x, str) for x in list(slab_name)]):
             raise GenerateSlabsErrors("Wrong type for elements of list: "
                                        "slab_name. Allowed types: str.")            
-
-
-
-                
-
+            
 class ReadSubWFsError(Exception):
-    """ Error in reading the subworkflow parameters
+    """ Error in reading the subworkflow parameters.
+    """
+    pass
+
+class NavigatorError(Exception):
+    """ Errors in database initialization and queries, for Navigator class.
+    """
+    pass
+
+class StuctNavigatorError(NavigatorError):
+    """ Errors in queries for structures handling in low and high level DBs.
+    """ 
+    pass
+
+class NavigatorMPError(Exception):
+    """ Errors in querying the online MP database.
     """
     pass
