f11c399f041281e05b95f83c83859bcdedb8d5b9
Gabriele Losi <gabriele.losi@outlook.com>
2021-02-09 16:18:13 +0100

    Add a FT for RelaxStructure

--- triboflow/firetasks/slabs.py
+++ triboflow/firetasks/slabs.py
@@ -17,18 +17,24 @@ __date__ = 'February 2nd, 2021'
 
 import os
 import monty
+from uuid import uuid4
 
 from fireworks.utilities.fw_utilities import explicit_serialize
 from fireworks import FiretaskBase, FWAction
+from fireworks import Workflow, Firework
 from atomate.utils.utils import env_chk
+from atomate.vasp.powerups import add_modify_incar
 from pymatgen.core.structure import Structure
 from pymatgen.core.surface import SlabGenerator
 from pymatgen.transformations.advanced_transformations import SlabTransformation
 
 from triboflow.utils.database import StructureNavigator, NavigatorMP
 from triboflow.firetasks.slabs_wfs import SlabWFs
-from triboflow.firetasks.errors import SlabThicknessError, GenerateSlabsError
+from triboflow.firetasks.surfene_wfs import SurfEneWfs
+from triboflow.utils.errors import SlabOptThickError, GenerateSlabsError, RelaxStructureError
 from triboflow.tasks.io import read_json
+from triboflow.utils.vasp_tools import GetCustomVaspRelaxSettings
+from atomate.vasp.fireworks.core import OptimizeFW, ScanOptimizeFW
 
 currentdir = os.path.dirname(__file__)
 
@@ -58,8 +64,8 @@ class FT_SlabOptThick(FiretaskBase):
 
     required_params = ['mp_id', 'miller', 'functional']
     optional_params = ['db_file', 'low_level', 'high_level', 'relax_type', 
-                       'convo_kind', 'nplane_start', 'nplane_incr', 'nsteps', 
-                       'bulk_name', 'slab_name',]
+                       'convo_kind', 'thick_start', 'thick_incr', 'nsteps',
+                       'vacuum', 'bulk_name', 'slab_name',]
 
     def run_task(self, fw_spec):
         """ Run the Firetask.
@@ -88,21 +94,83 @@ class FT_SlabOptThick(FiretaskBase):
             structure = Structure.from_dict(bulk.get('structure_fromMP'))
             comp_params = slab.get('comp_parameters', {})
 
-            wf = SlabWFs.select_slabthick_conv(structure=structure, 
-                                               mp_id=p['mp_id'], 
-                                               miller=p['miller'],
-                                               functional=p['functional'], 
-                                               comp_params=comp_params,
-                                               db_file=p['db_file'],
-                                               low_level=p['low_level'],
-                                               high_level=p['high_level'],
-                                               slab_name=p['slab_name'])
+            wf = self.select_slabthick_conv(structure=structure, 
+                                            mp_id=p['mp_id'], 
+                                            miller=p['miller'],
+                                            functional=p['functional'], 
+                                            comp_params=comp_params,
+                                            db_file=p['db_file'],
+                                            low_level=p['low_level'],
+                                            high_level=p['high_level'],
+                                            thick_start=p['thick_start'],
+                                            thick_incr=p['thick_incr'],
+                                            nsteps=p['nsteps'],
+                                            vacuum=p['vacuum'],
+                                            slab_name=p['slab_name'],
+                                            convo_kind=p['convo_kind'],
+                                            relax_type=p['relax_type'])
 
             return FWAction(detours=wf, update_spec=fw_spec)
 
         # Continue the Workflow
         else:
             return FWAction(update_spec=fw_spec)
+
+    def select_slabthick_conv(self, structure, mp_id, miller, comp_params, 
+                              db_file=None, low_level=None, high_level='triboflow', 
+                              thick_start=4, thick_incr=1, nsteps=6, vacuum=10, 
+                              slab_name=None, functional='PBE', 
+                              relax_type="slab_pos_relax", convo_kind='surfene'):
+        """
+        Select the desired subworkflow from the SlabWFs class, to converge the 
+        slab thickness either via surface energy or lattice parameter.
+
+        Parameters
+        ----------
+        structure : pymatgen.core.structure.Structure
+            Structure of the bulk to be processed.
+
+        mp_id : str
+            [description]
+
+        miller : str
+            [description]
+
+        comp_params : dict
+            [description]
+
+        functional : str, optional
+            [description], by default 'PBE'
+
+        convo_kind : str, optional
+            [description], by default 'surfene'
+
+        Returns
+        -------
+        [type]
+            [description]
+
+        Raises
+        ------
+        SlabThicknessError
+            When a wrong input argument is passed to convo_kind
+        """
+
+        if convo_kind == 'surfene':
+            generate_wf = SlabWFs.conv_slabthick_surfene
+        elif convo_kind == 'alat':
+            generate_wf = SlabWFs.conv_slabthick_alat
+        else:
+            raise SlabOptThickError("Wrong input argument for convo_kind. "
+                                    "Allowed options: 'surfene', 'alat'.")
+
+        wf = generate_wf(structure = structure, mp_id = mp_id, miller = miller,
+                        functional = functional, comp_params = comp_params,
+                        db_file=db_file, low_level=low_level, high_level=high_level,
+                        thick_start=thick_start, thick_incr=thick_incr, nsteps=nsteps,
+                        vacuum=vacuum, slab_name=slab_name, relax_type=relax_type)
+
+        return wf
 
 @explicit_serialize
 class FT_StartThickConvo(FiretaskBase):
@@ -115,37 +183,31 @@ class FT_StartThickConvo(FiretaskBase):
     required_params = ['structure', 'mp_id', 'miller', 'functional']
     optional_params = ['db_file', 'collection', 'comp_params', 'convo_kind',
                        'relax_type', 'thick_start', 'thick_incr', 'nsteps',
-                       'vacuum', 'slab_name', 'cluster_params']
+                       'vacuum', 'ext_index', 'slab_name', 'cluster_params']
 
     def run_task(self, fw_spec):
         """ Run the Firetask.
         """
+
+        # Define the json file containing default values and read parameters
+        dfl = currentdir + '/defaults_fw.json'
+        p = read_runtask_params(self, fw_spec, required_params, optional_params,
+                                default_file = dfl, default_key="StartThickConvo")
+
+        wf = SurfEneWfs.surface_energy_wf(structure=p['structure'], 
+                                          mp_id=p['mp_id'], 
+                                          miller=p['miller'], 
+                                          functional=p['functional'], 
+                                          db_file=p['db_file'], 
+                                          collection=p['collection'],
+                                          thick_start=p['thick_start'], 
+                                          thick_incr=p['thick_incr'], 
+                                          nsteps=p['nsteps'], 
+                                          vacuum=p['vacuum'], 
+                                          ext_index=p['ext_index'], 
+                                          slab_name=p['slab_name'])
         
-        min_thickness = 6
-        min_vacuum = 0
-
-        # nav_struct = StructureNavigator(p['db_file'], p['collection'])
-        # slab_db = nav_struct.get_slab_from_db(p['mp_id'], p['functional'], 
-        #                                       p['miller'], warning=True)
-
-
-        # Define the first Firetask 
-        names=None
-
-
-        FT_GenerateSlabs(strucure=p['structure'],
-                         mp_id=p['mp_id'],
-                         miller=p['miller'],
-                         functional=p['functional'],
-                         db_file=p[''],
-                         collection,
-                         thickness,
-                         vacuum,
-                         ext_index,
-                         symmetrize=False,
-                         slab_name=names)
-
-        #bulk.to(fmt='poscar', filename='POSCAR')
+        return FWAction(detours=wf, update_spec=fw_spec)
 
 @explicit_serialize
 class FT_EndThickConvo(FiretaskBase):
@@ -154,7 +216,7 @@ class FT_EndThickConvo(FiretaskBase):
     
     """
 
-    def run_task(elf, fw_spec):
+    def run_task(self, fw_spec):
         """ Run the Firetask.
         """ 
     pass
@@ -178,9 +240,9 @@ class FT_GenerateSlabs(FiretaskBase):
         dfl = currentdir + '/defaults_fw.json'
         p = read_runtask_params(self, fw_spec, required_params, optional_params,
                                 default_file = dfl, default_key="GenerateSlabs")
-        
-        GenerateSlabsError.check_thickness(p['thickness'])
-        GenerateSlabsError.check_slabname(p['slab_name'])
+
+        GenerateSlabsError.check_slabname_thickness(p['thickness'], 
+                                                    p['slab_name'])
 
         # Generate the slabs for each structure passed as input
         slabs = []
@@ -225,53 +287,103 @@ class FT_GenerateSlabs(FiretaskBase):
         
         return FWAction(update_spec=fw_spec)
 
+@explicit_serialize
+class FT_RelaxStructure(FiretaskBase):
+    """
+    Firetask description...
+    
+    """
+
+    required_params = ['mp_id', 'functional', 'struct_kind',]
+    optional_params = ['comp_params', 'miller', 'name', 'db_file', 'collection',
+                       'relax_type']
+
+    def run_task(self, fw_spec):
+        """ Run the Firetask.
+        """ 
+
+        # Define the json file containing default values and read parameters
+        dfl = currentdir + '/defaults_fw.json'
+        p = read_runtask_params(self, fw_spec, required_params, optional_params,
+                                default_file = dfl, default_key="RelaxStructure")
+
+        # Get the structure from the Database with a calculation tag
+        structure, tag = self.extract_data_from_db()
+        if p['name'] is not None:
+            structure = structure['name']
+
+        # Set the simulation if the calculation is not already at convergence
+        if 'relaxed' or 'relaxed_slab' not in structure:
+            fw = self.set_calculation(structure, tag, dfl)
+
+            # Set the Relaxation Workflow for making a detour.
+            # Use add_modify_incar powerup to add KPAR and NCORE settings based on 
+            # env_chk in my_fworker.yaml.
+            wf_name = tag.split('_')[0] + p['relax_type']
+            opt_wf = add_modify_incar(Workflow([fw], name=wf_name))
+
+            return FWAction(detours=opt_wf)
+    
+    def extract_data_from_db(self):
+
+        RelaxStructureError.check_struct_kind(self.p['struct_kind'])
+
+        # Call the navigator for retrieving structure
+        nav_struct = StructureNavigator(db_file=self.p['db_file'],
+                                        high_level=self.p['high_level'])
+
+        # Extract data from database
+        if self.p['struct_kind'] == 'bulk':
+            structure = nav_struct.get_bulk_from_db(self.p['mp_id'], 
+                                                    self.p['functional'],
+                                                    warning=True)
+        elif self.p['struct_kind'] == 'slab':
+            structure = nav_struct.get_slab_from_db(self.p['mp_id'], 
+                                                    self.p['functional'], 
+                                                    self.p['miller'], 
+                                                    warning=True)
+        elif self.p['struct_kind'] == 'interface':
+            structure = nav_struct.get_interface_from_db(self.p['name'],
+                                                         self.p['functional'],
+                                                         warning=True)
+        
+        RelaxStructureError.is_data(structure=structure, mp_id=self.p['mp_id'], 
+                                    functional=self.p['functional'], 
+                                    struct_kind=self.p['struct_kind'])
+        
+        # Get a tag for the calculation
+        formula = structure.composition.reduced_formula
+        if self.p['miller'] is not None:
+            tag = formula + str(p['miller']) + '_' + str(uuid4())
+        else:
+            tag = formula + '_' + str(uuid4())
+
+        return structure, tag
+
+    def set_calculation(self, structure, tag, dfl):
+
+        # Check the computational parameters
+        comp_params = self.p['comp_params']
+        if not comp_params:
+            defaults = read_json(dfl)
+            comp_params = defaults['comp_params']
+
+        # Set options for vasp
+        vis = GetCustomVaspRelaxSettings(structure, comp_params,
+                                         self.p['relax_type'])
+        
+        # Create the Firework to run the simulation
+        if self.p['functional'] == 'SCAN':
+            fw = ScanOptimizeFW(structure=structure, name=tag, vasp_input_set=vis)
+        else:
+            fw = OptimizeFW(structure, name=tag, vasp_input_set=vis,
+                            half_kpts_first_relax=True)
+
+        return fw
+
 # ============================================================================
 # Functions
 # ============================================================================
-
-def select_slabthick_conv(structure, mp_id, miller, comp_params, 
-                          functional='PBE', convo_kind='surfene'):
-    """
-    [summary]
-
-    Parameters
-    ----------
-    structure : [type]
-        [description]
-    mp_id : [type]
-        [description]
-    miller : [type]
-        [description]
-    comp_params : [type]
-        [description]
-    functional : str, optional
-        [description], by default 'PBE'
-    convo_kind : str, optional
-        [description], by default 'surfene'
-
-    Returns
-    -------
-    [type]
-        [description]
-
-    Raises
-    ------
-    SlabThicknessError
-        [description]
-    """
-
-    if convo_kind == 'surfene':
-        generate_wf = SlabWFs.conv_slabthick_surfene
-    elif convo_kind == 'alat':
-        generate_wf = SlabWFs.conv_slabthick_alat
-    else:
-        raise SlabThicknessError("Wrong input argument for convo_kind. "
-                                 "Allowed options: 'surfene', 'alat'.")
-
-    wf = generate_wf(structure = structure, flag = mp_id, miller = miller,
-                      comp_parameters = comp_params, functional = functional, 
-                      print_help = False)
-    return wf
 
 def read_runtask_params(obj, fw_spec, required_params, optional_params,
                         default_file, default_key):
@@ -312,22 +424,3 @@ def read_runtask_params(obj, fw_spec, required_params, optional_params,
         params['db_file'] = env_chk('>>db_file<<', fw_spec)
 
     return params
-
-
-# # Read required parameters
-# flag = self['mp_id']
-# miller = self['miller']
-# functional = self['functional']
-
-# # Set optional parameters
-# db_file = self.get('db_file')
-# if not db_file:
-#     db_file = env_chk('>>db_file<<', fw_spec)
-# high_level = self.get('high_level', None)
-# relax_type = self.get('relax_type', 'slab_pos_relax')
-# convo_kind = self.get('convo_kind', 'surfene')
-
-# slab_name = self.get('slab_struct_name', 'unrelaxed_slab')
-
-# bulk_name = self.get('bulk_struct_name', 'structure_equiVol')
-# slab_out_name = self.get('slab_out_name', 'relaxed_slab')
