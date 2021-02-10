28c84bb879d24442b37532617f9b6a9d3d0b0e93
mwo <m.wolloch@gmail.com>
2021-02-10 17:43:41 +0100

    Updated the PES_test file a little.

--- triboflow/tests/PES_test.py
+++ triboflow/tests/PES_test.py
@@ -12,7 +12,7 @@ from fireworks import LaunchPad
 from triboflow.workflows.subworkflows import CalcPES_SWF
 from triboflow.utils.structure_manipulation import SlabFromStructure
 from triboflow.utils.database import GetDBJSON, GetSlabFromDB, \
-    GetLowEnergyStructure
+    GetLowEnergyStructure, GetInterfaceFromDB
 
 db_file = GetDBJSON()
 functional = "SCAN"
@@ -24,21 +24,28 @@ functional = "SCAN"
 # slab = Slab.from_dict(Slab_dict['relaxed_slab'])
 # comp_params = Slab_dict['comp_parameters']
 
-#Test GrapheneGraphene interface
-struct, mpid = GetLowEnergyStructure('C', 'mp-1040425')
-slab = SlabFromStructure([0,0,1], struct)
-comp_params = {'functional': functional,
-               'use_vdw': True,
-               'use_spin': True,
-               'encut': 500,
-               'is_metal': True,
-               'k_dens': 2000
-               }
+#Test Au111_C001 interface
+int_dict = GetInterfaceFromDB('Au111_C001_mp-66_mp-81', db_file, functional)
+top_slab = Slab.from_dict(int_dict['top_aligned'])
+bottom_slab = Slab.from_dict(int_dict['bottom_aligned'])
+comp_params = int_dict['comp_parameters']
 
-WF = CalcPES_SWF(top_slab=slab, bottom_slab=slab, comp_parameters=comp_params,
+#Test GrapheneGraphene interface
+# struct, mpid = GetLowEnergyStructure('C', 'mp-1040425')
+# slab = SlabFromStructure([0,0,1], struct)
+# comp_params = {'functional': functional,
+#                'use_vdw': True,
+#                'use_spin': True,
+#                'encut': 500,
+#                'is_metal': True,
+#                'k_dens': 2000
+#                }
+
+WF = CalcPES_SWF(top_slab=top_slab, bottom_slab=bottom_slab, top_mpid='mp-66',
+                 bottom_mpid='mp-81',comp_parameters=comp_params,
                  file_output=True, output_dir = '/home/fs71332/mwo4',
                  remote_copy = True, server = 'vsc4.vsc.ac.at',  user = 'mwo4', 
                  port = 27)
 
-lpad = LaunchPad.auto_load()
-lpad.add_wf(WF)
+#lpad = LaunchPad.auto_load()
+#lpad.add_wf(WF)
