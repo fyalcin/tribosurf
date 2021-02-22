a8d240b7efa29f479cd959af17d9ab645b64ba8e
Gabriele Losi <gabriele.losi@outlook.com>
2021-02-22 18:23:46 +0100

    Add a test to MoveTagResults

---
+++ triboflow/firetasks/tests/test_core.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 16:56:54 2021
+
+The the Firetasks of the `core` module.
+
+Author: Gabriele Losi (glosi000)
+Copyright 2021, Prof. M.C. Righi, TribChem, University of Bologna
+
+"""
+
+from pymatgen.core.surface import Structure
+from fireworks import Firework, Workflow, LaunchPad
+from fireworks.core.rocket_launcher import rapidfire
+
+from triboflow.firetasks.core import FT_MoveTagResults
+
+
+# Define input parameters
+mp_id = 'mp-126'
+collection_from = 'tasks'
+collection_to = 'PBE.bulk_data'
+tag = True
+tag_key = 'transfer_test'
+db_file = None
+database_from = None
+database_to = 'tribchem'
+miller = [1, 2, 3]
+entry_check = None
+entry_to = ['test', 'data', 'energy']
+entry_from = ['data', 'energy']
+override = False
+cluster_params = {}
+
+# Instantiate the Firetask and create a WF
+ft = FT_MoveTagResults(mp_id=mp_id,
+                       collection_from=collection_from,
+                       collection_to=collection_to,
+                       tag=tag,
+                       tag_key=tag_key,
+                       db_file=db_file,
+                       database_from=database_from,
+                       database_to=database_to,
+                       miller=miller,
+                       entry_check=entry_check,
+                       entry_to=entry_to,
+                       entry_from=entry_from,
+                       override=override,
+                       cluster_params=cluster_params)
+                       
+wf = Workflow([Firework([ft])])
+
+# Run the workflow
+lpad = LaunchPad.auto_load()
+lpad.add_wf(wf)
+rapidfire(lpad)
--- triboflow/firetasks/tests/test_slabs.py
+++ triboflow/firetasks/tests/test_slabs.py
@@ -3,42 +3,41 @@
 """
 Created on Mon Feb 22 16:27:12 2021
 
-The the Firetasks of the `slabs` module.
+Test the Firetasks of the `slabs` module.
 
 Author: Gabriele Losi (glosi000)
 Copyright 2021, Prof. M.C. Righi, TribChem, University of Bologna
+
 """
+
+from pymatgen.core.surface import Structure
+from fireworks import Firework, Workflow, LaunchPad
+from fireworks.core.rocket_launcher import rapidfire
 
 from triboflow.firetasks.slabs import FT_GenerateSlabs
 from triboflow.utils.database import StructureNavigator
-import os
 
-from fireworks import LaunchPad
-from fireworks.core.rocket_launcher import rapidfire
 
-from pymatgen.core.surface import Structure
-
-from fireworks import Firework, Workflow
-
-currentdir = os.path.dirname(__file__)
-
+# Define input parameters
 mp_id = 'mp-126'
 functional = 'PBE'
-miller = [[1, 0, 0], [3, 3, 3], [1, 2, 2], [11, 0, 0]]
+miller = [1, 2, 3]
 db_file = None
-high_level = "tribchem"
+high_level = 'tribchem'
 database = high_level
-thickness = [12,12,12,12]
+thickness = 10
 thick_max = 14
-vacuum=[10, 10, 10, 10]
-ext_index=0
-in_unit_planes=True
-slab_name=['unrelaxed', 'unrelaxed', 'unrelaxed', 'unrelaxed']
+vacuum = 10
+ext_index = 0
+in_unit_planes = True
+slab_name = 'unrelaxed'
 
+# Call the Navigator and retrieve the bulk structure
 nav = StructureNavigator(db_file, high_level)
 structure = nav.get_bulk_from_db(mp_id, functional, warning=True)['structure_fromMP']
 structure = Structure.from_dict(structure)
 
+# Instantiate the Firetask and create a WF
 ft = FT_GenerateSlabs(structure=structure,
                       mp_id=mp_id,
                       miller=miller,
@@ -52,9 +51,9 @@ ft = FT_GenerateSlabs(structure=structure,
                       ext_index=ext_index,
                       in_unit_planes=in_unit_planes,
                       entry=slab_name)
-
 wf = Workflow([Firework([ft])])
 
+# Run the workflow
 lpad = LaunchPad.auto_load()
 lpad.add_wf(wf)
 rapidfire(lpad)
