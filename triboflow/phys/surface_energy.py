92d0826d1bb1dfe2c5b8cab2e374018e12c18c19
Gabriele Losi <gabriele.losi@outlook.com>
2021-02-22 16:11:59 +0100

    Add surface energy to phys modules

---
+++ triboflow/phys/surface_energy.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 11:00:54 2021
+
+
+@author: glosi000
+"""
+
+__author__ = 'Gabriele Losi'
+__copyright__ = 'Copyright 2021, Prof. M.C. Righi, TribChem, University of Bologna'
+__contact__ = 'clelia.righi@unibo.it'
+__date__ = 'February 22nd, 2021'
+
+import numpy as np
+
+def calculate_surface_energy(output_list, sym_surface=True):
+    """
+    Calculate the surface energy by passing a list containing all the dictionary
+    with the output data. The first element is treated to be the bulk oriented
+    along a specific miller index direction.
+
+    Parameters
+    ----------
+    output_list : list of dict
+        List of dictionaries where each one contains the output of a VASP
+        simulation. Basically output_list can be whatever dictionary, fundamental
+        keys that should be present are: structure, energy, energy_per_atom.
+    
+    sym_surface : bool, optional
+        If the surfaces have to be considered symmetric or not.
+    
+    Returns
+    -------
+    surfene : list of floats
+        List containing the surface energies calculated from output_list.
+    """
+
+    # Take out the energy per atom of the bulk
+    energy_per_atom = output_list[0]['energy_per_atom']
+
+    # Loop over the slab elements of output_list and calculate surface energy
+    surfene = np.array([])
+    for d in output_list[1:]:
+        energy = d['energy']
+        nsites = d['nsites']
+        surfene.append(energy - energy_per_atom * nsites)
+
+    # Divide the surface energies by two if the surfaces are symmetric
+    if sym_surface:
+        surfene /= 2.
+
+    return surfene
