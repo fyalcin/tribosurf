42d1985cb1d4c8e40c536fbf437aca0580302a38
Omar Chehaimi <omar.chehaimi@outlook.it>
2021-03-04 17:26:09 +0100

    WIP surfene test

---
+++ triboflow/workflows/tests/surfene_test.py
@@ -0,0 +1,37 @@
+"""
+Created on Thu Mar 4 11:36:15 2021
+
+Tests for surface energy calculation.
+The following tests are about the slabs generation and the following surface 
+energy calculation for an aluminum system.
+        
+Author: Omar Chehaimi
+Copyright 2021, Prof. M.C. Righi, TribChem, ERC-SLIDE, University of Bologna
+
+"""
+
+__author__ = 'Omar Chehaimi'
+__copyright__ = 'Copyright 2021, Prof. M.C. Righi, TribChem, ERC-SLIDE, University of Bologna'
+__contact__ = 'clelia.righi@unibo.it'
+__date__ = 'March 4th, 2021'
+
+import os
+from pathlib import Path, PurePosixPath
+
+from fireworks import LaunchPad
+from fireworks.core.rocket_launcher import rapidfire
+
+from triboflow.utils.database import NavigatorMP
+from triboflow.workflows.surfene_wfs import SurfEneWF
+
+# Get the bulk
+formula = 'Al'
+mpid = 'mp-134'
+nav_mp = NavigatorMP()
+
+structure, mpid = nav_mp.get_low_energy_structure(
+    chem_formula=formula, 
+    mp_id=mpid)
+
+# Surface generation tests
+#wf = SurfEneWF.conv_surface_energy()
