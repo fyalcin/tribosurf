8b5119fbc6594ed8439b08c01957d09d1e0aa3cd
Gabriele Losi <gabriele.losi@outlook.com>
2021-04-06 10:10:15 +0200

    Add surfene wf test

---
+++ triboflow/workflows/tests/surfene_test.py
@@ -0,0 +1,47 @@
+"""
+Created on Thu Mar 4 11:36:15 2021
+
+Tests for surface energy calculation.
+The following tests are about the slabs generation and the following surface 
+energy calculation for an aluminum system.
+        
+Author: Omar Chehaimi
+Copyright 2021, Prof. M.C. Righi, TribChem, ERC-SLIDE, University of Bologna
+
+"""
+
+__author__ = 'Omar Chehaimi'
+__copyright__ = 'Copyright 2021, Prof. M.C. Righi, TribChem, ERC-SLIDE, University of Bologna'
+__contact__ = 'clelia.righi@unibo.it'
+__date__ = 'March 4th, 2021'
+
+import os
+from pathlib import Path, PurePosixPath
+
+from fireworks import LaunchPad
+from fireworks.core.rocket_launcher import rapidfire
+
+from triboflow.utils.database import NavigatorMP
+from triboflow.workflows.surfene_wfs import SurfEneWF
+
+# Get the bulk
+formula = 'Cu'
+mid = 'mp-30'
+nav_mp = NavigatorMP()
+
+structure, mid = nav_mp.get_low_energy_structure(
+    chem_formula=formula, 
+    mp_id=mid)
+
+# Surface generation tests
+wf = SurfEneWF.conv_surface_energy(
+    structure=structure, 
+    mp_id=mid, 
+    miller=[1, 0, 0],
+    thick_min=1, 
+    thick_max=1,
+    thick_incr=1)
+
+# Launch the calculation
+lpad = LaunchPad.auto_load()
+lpad.add_wf(wf)
