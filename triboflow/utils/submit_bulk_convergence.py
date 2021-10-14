1c86dd57ecfe29cbb82c799a96be1d5c5175623a
mwolloch <michael.wolloch@univie.ac.at>
2021-10-14 18:00:45 +0200

    added utilities to add many full heterogeneous workflows and bulk convergence workflows at once.

---
+++ triboflow/utils/submit_bulk_convergence.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 16:12:32 2021
+
+@author: wolloch
+"""
+
+from fireworks import LaunchPad
+from fireworks import Workflow, Firework
+from triboflow.fireworks.init_fws import InitWF
+from triboflow.firetasks.structure_manipulation import FT_StartPreRelax
+from triboflow.firetasks.init_check import material_from_mp
+from triboflow.firetasks.convergence import FT_StartConvo
+from triboflow.utils.utils import load_homoatomic_materials
+
+computational_params = {'functional': 'PBE',
+                   'volume_tolerance': 0.001,
+                   'BM_tolerance': 0.01,
+                   'use_vdw': False,
+                   'surfene_thr': 0.01,
+                   'vacuum': 15}
+
+def get_bulk_convergence_wf(material, comp_params):
+    struct, mp_id = material_from_mp(material)
+    functional = comp_params.get('functional', 'PBE')
+    
+    WF = []
+
+    Initialize = InitWF.checkinp_bulk_convo(material=material,
+                                            computational=comp_params)
+    WF.append(Initialize)
+
+    PreRelaxation = Firework(FT_StartPreRelax(mp_id=mp_id,
+                                              functional=functional),
+                                 name='Start pre-relaxation for {}'
+                                .format(material['formula']))
+    WF.append(PreRelaxation)
+    
+    ConvergeEncut = Firework(FT_StartConvo(conv_type='encut',
+                                           mp_id=mp_id,
+                                           functional=functional,
+                                           ),
+                                name='Start encut convergence for {}'
+                                      .format(material['formula']))
+    WF.append(ConvergeEncut)
+    
+    ConvergeKpoints = Firework(FT_StartConvo(conv_type='kpoints',
+                                           mp_id=mp_id,
+                                           functional=functional,
+                                           ),
+                                name='Start kpoints convergence for {}'
+                                      .format(material['formula']))
+    WF.append(ConvergeKpoints)
+    
+    Dependencies = {Initialize: [PreRelaxation],
+                    PreRelaxation: [ConvergeEncut],
+                    ConvergeEncut: [ConvergeKpoints]}
+
+    WF_Name = 'ConvergeBulk ' + material['formula'] +' '+ mp_id +' '+ functional
+
+    WF = Workflow(WF, Dependencies, name=WF_Name)
+    
+    return WF
+
+def submit_multiple_wfs(workflow_list):    
+    lpad = LaunchPad.auto_load()
+    for wf in workflow_list:
+        lpad.add_wf(wf)
+
+
+
+if __name__ == "__main__":
+    materials_dict = load_homoatomic_materials()
+    materials_list =[]
+
+    for formula in ['Al', 'C', 'Si', 'Ge', 'Cu', 'Ag', 'Au', 'Ni', 'Fe', 'Ti', 'Co']:
+        mpid = materials_dict[formula]['mpids'][materials_dict[formula]['default']]
+        materials_list.append({'formula': formula, 'mpid': mpid})
+        workflow_list = []
+    
+    for mat in materials_list:
+        workflow_list.append(get_bulk_convergence_wf(mat, computational_params))
+    #submit_multiple_wfs(workflow_list)
---
+++ triboflow/utils/submit_full_hetero_wfs.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 16:12:32 2021
+
+@author: wolloch
+"""
+
+from fireworks import LaunchPad
+from triboflow.workflows.main import heterogeneous_wf
+
+materials = {'Al': {'mpid': 'mp-134'},
+             'Cu': {'mpid': 'mp-30'},
+             'Fe': {'mpid': 'mp-13'}}
+             #'C': {'mpid': 'mp-66',
+              #     'thick_min': 4,
+              #     'thick_inc': 2}}
+
+miller_indices = ['100', '110', '111']
+
+default_thickness_params = {'thick_min': 4,
+                            'thick_max': 14,
+                            'thick_incr': 1,}
+
+computational_params = {'functional': 'SCAN',
+                   'volume_tolerance': 0.001,
+                   'BM_tolerance': 0.01,
+                   'use_vdw': False,
+                   'surfene_thr': 0.01,
+                   'vacuum': 15}
+
+interface_params = {'max_area': 100,
+                    'r1r2_tol': 0.1,
+                    'max_mismatch': 0.025,
+                    'max_angle_diff': 1.0}
+
+
+def create_triboflow_inputs(materials,
+                            miller_indices,
+                            default_thickness_params,
+                            computational_params,
+                            interface_params):
+    
+    slabs = []
+    for k, v in materials.items():
+        mpid = v.pop('mpid')
+        for miller in miller_indices:
+            m1 = {'formula': k,
+                  'mpid': mpid,
+                  'miller': miller}
+            m1.update(default_thickness_params)
+            m1.update(v)
+            slabs.append(m1)
+        
+        inputs_list = []
+        for mat_1 in slabs:
+            for mat_2 in slabs:
+                if not mat_1 == mat_2:
+                    inputs = {'material_1': mat_1,
+                              'material_2': mat_2,
+                              'computational_params': computational_params,
+                              'interface_params': interface_params}
+                    dummy_inputs = {'material_1': mat_2,
+                                    'material_2': mat_1,
+                                    'computational_params': computational_params,
+                                    'interface_params': interface_params}
+                    if dummy_inputs not in inputs_list:
+                        inputs_list.append(inputs)
+    return inputs_list
+    
+def submit_multiple_wfs(inputs_list):    
+    lpad = LaunchPad.auto_load()
+    for inputs in inputs_list:
+        WF = heterogeneous_wf(inputs)
+        lpad.add_wf(WF)
+
+if __name__ == "__main__":
+    inputs_list = create_triboflow_inputs(materials=materials,
+                                          miller_indices=miller_indices,
+                                          computational_params=computational_params,
+                                          interface_params=interface_params,
+                                          default_thickness_params=default_thickness_params)
+    submit_multiple_wfs(inputs_list)
