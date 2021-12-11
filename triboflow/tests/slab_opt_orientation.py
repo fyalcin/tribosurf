2bd60fa32d7bcccb76ddef234f36949f21a2078a
firaty <firat.yalcin@univie.ac.at>
2021-12-11 11:56:34 +0100

    Cleaned up the test file for SlabOptOrientation a bit.

--- triboflow/tests/slab_opt_orientation.py
+++ triboflow/tests/slab_opt_orientation.py
@@ -15,14 +15,12 @@ functional = 'PBE'
 db_file = 'auto'
 high_level = True
 comp_params = {}
-
-max_index = 3
-
 override = True
+fake = False
 
 sg_params = {'miller': [(3, 1, 1)],
              'symmetrize': False,
-             'slab_thick': 10,
+             'slab_thick': 15,
              'vac_thick': 15,
              'prim': True,
              'lll_reduce': True,
@@ -33,41 +31,7 @@ sg_params = {'miller': [(3, 1, 1)],
 
 sg_filter = {'method': 'all'}
 
-bvs_method = 'all'
-bvs_param = 1
-fake = False
-
 lpad = LaunchPad.auto_load()
-
-# for lll in [True, False]:
-#     for prim in [True, False]:
-#         for mns in [1, 'max']:
-#             for sym in [True, False]:
-#                 sg_params = {'symmetrize': sym,
-#                              'slab_thick': 15,
-#                              'vac_thick': 15,
-#                              'prim': prim,
-#                              'lll_reduce': lll,
-#                              'minimize_bv': True,
-#                              'tol': 0.1,
-#                              'mns': mns}
-#
-#                 FT = FT_SlabOptOrientation(mpid=mpid,
-#                                            functional=functional,
-#                                            db_file=db_file,
-#                                            max_index=max_index,
-#                                            high_level=high_level,
-#                                            comp_params=comp_params,
-#                                            bvs_method=bvs_method,
-#                                            sg_params=sg_params,
-#                                            bvs_param=bvs_param,
-#                                            override=override,
-#                                            fake=fake)
-#
-#                 FW = Firework(FT)
-#                 WF1 = Workflow.from_Firework(FW)
-#
-#                 lpad.add_wf(WF1)
 
 FT = FT_SlabOptOrientation(mpid=mpid,
                            functional=functional,
