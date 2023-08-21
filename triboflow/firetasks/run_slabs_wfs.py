17ec2e388cd9ec41a29a87d4ab7e86a5a18fe664
firaty <firat.yalcin@univie.ac.at>
2023-08-21 16:48:29 +0200

    GetSlabSurfenListFromUids now flips the slabs if the bottom termination has the lower surface energy, and puts everything about the slabs calculated into the spec, with the material index affixed.

--- triboflow/firetasks/run_slabs_wfs.py
+++ triboflow/firetasks/run_slabs_wfs.py
@@ -44,6 +44,7 @@ from hitmen_utils.db_tools import VaspDB
 from hitmen_utils.misc_tools import check_input
 from hitmen_utils.shaper import Shaper
 from surfen.firetasks.start_swfs import StartSurfaceEnergy
+from triboflow.phys.interface_matcher import flip_slab
 from triboflow.utils.database import (
     Navigator,
     StructureNavigator,
@@ -894,10 +895,16 @@ class FT_EndThickConvo(FiretaskBase):
 
 
 @explicit_serialize
-class GetSurfaceEnergiesFromUids(FiretaskBase):
+class GetSlabSurfenListFromUids(FiretaskBase):
     _fw_name = "Query the surface energies with the list of uids provided"
     required_params = []
-    optional_params = ["db_file", "high_level", "fake_calc", "surfen_coll"]
+    optional_params = [
+        "db_file",
+        "high_level",
+        "fake_calc",
+        "surfen_coll",
+        "material_index",
+    ]
 
     def run_task(self, fw_spec):
         req_inp = {k: self.get(k) for k in self.required_params}
@@ -907,17 +914,10 @@ class GetSurfaceEnergiesFromUids(FiretaskBase):
         prev_swf_info = fw_spec["StartSurfaceEnergy_info"]
         mpid, uids = prev_swf_info["mpid"], prev_swf_info["uids"]
         nav = VaspDB(db_file=inp["db_file"], high_level=inp["high_level"])
-        results_dict = {
-            result["uid"]: result
-            for result in list(
-                nav.find_many_data(
-                    inp["surfen_coll"],
-                    {"uid": {"$in": uids}},
-                    {"calcs": 0},
-                )
-            )
-        }
-        for uid, result in results_dict.items():
+        results = list(
+            nav.find_many_data(inp["surfen_coll"], {"uid": {"$in": uids}}, {"calcs": 0})
+        )
+        for result in results:
             slab = Slab.from_dict(result["structure"])
             hkl = result["hkl"]
             uid = result["uid"]
@@ -936,8 +936,28 @@ class GetSurfaceEnergiesFromUids(FiretaskBase):
                 upsert=True,
             )
 
-        # Update the spec
-        fw_spec["GetSurfaceEnergiesFromUids_info"] = results_dict
+        slab_surfen_list = []
+        for slab_dict in results:
+            slab = Slab.from_dict(slab_dict["fully_relaxed_slab"])
+            surfen_dict = slab_dict["surface_energy"]
+            slab_params = slab_dict["slab_params"]
+            surfen_min = min(surfen_dict.values())
+
+            if not slab_params.get("sym", False):
+                surfen_min_key = min(surfen_dict, key=surfen_dict.get)
+                if surfen_min_key == "bottom":
+                    slab = flip_slab(slab)
+
+            slab_surfen_list.append(
+                {
+                    "structure": slab,
+                    "surface_energy": surfen_min,
+                    "slab_params": slab_params,
+                }
+            )
+
+        slab_surfen_list.sort(key=lambda x: x["surface_energy"])
+        fw_spec[f"slab_surfen_list_{inp['material_index']}"] = slab_surfen_list
 
         return FWAction(update_spec=fw_spec)
 
@@ -948,6 +968,7 @@ class RunSurfenSwfGetEnergies(FiretaskBase):
     required_params = ["mpid"]
     optional_params = [
         "comp_params",
+        "comp_params_from_db",
         "sg_params",
         "sg_filter",
         "db_file",
@@ -956,6 +977,7 @@ class RunSurfenSwfGetEnergies(FiretaskBase):
         "surfen_coll",
         "bulk_coll",
         "add_full_relax",
+        "material_index",
     ]
 
     def run_task(self, fw_spec):
@@ -963,11 +985,38 @@ class RunSurfenSwfGetEnergies(FiretaskBase):
         opt_inp = {k: self.get(k) for k in self.optional_params}
         opt_inp = check_input(opt_inp, self.optional_params)
         inp = {**req_inp, **opt_inp}
+        mpid = inp["mpid"]
+        comp_params = inp["comp_params"]
+        sg_params = inp["sg_params"]
+        # create a name_suffix variable and use miller or max_index from sg_params, whichever is available
+        name_suffix = ""
+        if sg_params.get("miller", False):
+            name_suffix = (
+                f"Miller {sg_params['miller']} - Material Index {inp['material_index']}"
+            )
+        elif sg_params.get("max_index", False):
+            name_suffix = (
+                f"MMI {sg_params['max_index']} - Material Index {inp['material_index']}"
+            )
+        # check if comp_params_loc is given
+        if inp["comp_params_from_db"]:
+            nav = VaspDB(db_file=inp["db_file"], high_level=inp["high_level"])
+            bulk_coll = inp["bulk_coll"]
+
+            bulk_data = nav.find_data(collection=bulk_coll, fltr={"mpid": mpid})
+            if bulk_data:
+                comp_params_db = bulk_data["comp_parameters"]
+            else:
+                comp_params_db = None
+
+            if comp_params_db:
+                comp_params.update(comp_params_db)
+
         fw1 = Firework(
             [
                 StartSurfaceEnergy(
-                    mpid=inp["mpid"],
-                    comp_params=inp["comp_params"],
+                    mpid=mpid,
+                    comp_params=comp_params,
                     sg_params=inp["sg_params"],
                     sg_filter=inp["sg_filter"],
                     db_file=inp["db_file"],
@@ -975,26 +1024,26 @@ class RunSurfenSwfGetEnergies(FiretaskBase):
                     custom_id=inp["custom_id"],
                     surfen_coll=inp["surfen_coll"],
                     bulk_coll=inp["bulk_coll"],
-                    add_full_relax=inp["add_full_relax"],
+                    add_full_relax=True,
                 )
             ],
-            name="StartSurfaceEnergy",
+            name=f"Run Surface Energy Workflow for {mpid} {name_suffix}",
         )
 
         fw2 = Firework(
             [
-                GetSurfaceEnergiesFromUids(
+                GetSlabSurfenListFromUids(
                     db_file=inp["db_file"],
                     high_level=inp["high_level"],
                     surfen_coll=inp["surfen_coll"],
                 )
             ],
-            name="GetSurfaceEnergiesFromUids",
+            name=f"Get slabs and corresponding surface energies for {mpid} {name_suffix}",
             parents=fw1,
         )
 
         wf = Workflow(
             [fw1, fw2],
-            name=f"Run Surface Energy Workflow and Get Surface Energies for {inp['mpid']}",
+            name=f"Run Surface Energy Workflow and Extract Surface Energies for {mpid} {name_suffix}",
         )
         return FWAction(detours=wf)
