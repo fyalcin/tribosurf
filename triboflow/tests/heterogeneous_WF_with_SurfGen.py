c06ae5234fbcfdada5e6ffad9155f2c174cf3bd3
Michael Wolloch <michael.wolloch@univie.ac.at>
2023-08-29 15:58:06 +0200

    updated test for heterogeneous WF

--- triboflow/tests/heterogeneous_WF_with_SurfGen.py
+++ triboflow/tests/heterogeneous_WF_with_SurfGen.py
@@ -12,7 +12,7 @@ from triboflow.workflows.main import heterogeneous_wf_with_surfgen
 
 inputs = {
     "material_1": {"formula": "Si", "mpid": "mp-149"},
-    "material_2": {"formula": "Si", "mpid": "mp-149"},
+    "material_2": {"formula": "Al", "mpid": "mp-134"},
     "computational_params": {
         "functional": "PBE",
         "volume_tolerance": 0.01,
@@ -23,31 +23,34 @@ inputs = {
     "sg_params_1": {
         "slab_thick": 4,
         "vac_thick": 20.0,
-        "min_thick_A": 2.0,
-        "miller": (1, 0, 0),
+        "min_thick_A": 3.0,
+        "max_index": 2,
     },
     "sg_params_2": {
         "slab_thick": 4,
         "vac_thick": 20.0,
-        "min_thick_A": 2.0,
-        "miller": (1, 0, 0),
+        "min_thick_A": 3.0,
+        "max_index": 2,
     },
-    "sg_filter_1": {"method": "all"},
-    "sg_filter_2": {"method": "all"},
+    "sg_filter_1": {"method": "bvs_min_N",
+                    "bvs_param": 10},
+    "sg_filter_2": {"method": "bvs_min_N",
+                    "bvs_param": 10},
     "interface_params": {
         "max_area": 100,
         "max_area_ratio_tol": 0.1,
         "max_length_tol": 0.05,
+        "pressure": 1,
     },
-    "db_file": "/home/yalcin/config_local/db.json",
-    "high_level": "surfflow_test",
+    "db_file": "/home/fs71411/mwo3/FireWorks/config_VSC5_Zen3/db.json",
+    "high_level": "release_test_db",
 }
 
 WF = heterogeneous_wf_with_surfgen(inputs)
 
-lpad = LaunchPad.from_file("/home/yalcin/config_local/my_launchpad.yaml")
-today = datetime.today().strftime("%Y-%m-%d")
-lpad.reset(today)
+lpad = LaunchPad.auto_load()
+#today = datetime.today().strftime("%Y-%m-%d")
+#lpad.reset(today)
 lpad.add_wf(WF)
 
 # fworker = FWorker.from_file("/home/yalcin/config_local/my_fworker.yaml")
